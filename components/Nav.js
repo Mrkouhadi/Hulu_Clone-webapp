4bffb2e14f955339125db8ffff69422f43eaf536
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-05-21 20:42:47 +0800

    added Nav component and import it in index.js

---
+++ components/Nav.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Nav = () => {
+  return <nav>
+            hello
+         </nav>
+}
+export default Nav
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
 
-export default function Home() {
+const Home =()=> {
   return (
     <div className="h-screen">
       <Head>
@@ -12,9 +13,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* header */}
       <Header/>
-     
+      <Nav/>
+
     </div>
   )
 }
+export default  Home;
