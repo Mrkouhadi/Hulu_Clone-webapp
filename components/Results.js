e2e2472184846ef223a7818597790d8c957a9f17
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-05-24 20:53:13 +0800

    add grid and flex css in results wrapper

--- components/Results.js
+++ components/Results.js
@@ -4,7 +4,7 @@ import Thumbnail from './Thumbnail'
 const Results = ({requests}) => {
 
   return (
-    <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center '>
+    <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
         {
             requests.map(res => (
                 <Thumbnail key={res.id} result={res} />
